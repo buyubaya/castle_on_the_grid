3a5eca89e4f4535fbef11d081775c56a5ec94bc3
buyubaya <hihihi_caigikia@yahoo.com>
2019-01-19 22:37:35 -0800

    Updates

---
+++ precache-manifest.8eed94d1c0e8eb70ddb8ea63abb34d0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc5d9e1de26c46e472c2",
+    "url": "/castle_on_the_grid/static/js/runtime~main.cc5d9e1d.js"
+  },
+  {
+    "revision": "dc0061a00f99fa2e34c1",
+    "url": "/castle_on_the_grid/static/js/main.dc0061a0.chunk.js"
+  },
+  {
+    "revision": "0618129449fa37b7aae9",
+    "url": "/castle_on_the_grid/static/js/1.06181294.chunk.js"
+  },
+  {
+    "revision": "dc0061a00f99fa2e34c1",
+    "url": "/castle_on_the_grid/static/css/main.1cfbb070.chunk.css"
+  },
+  {
+    "revision": "89d0ba33380d4dc15c007543a4542f2f",
+    "url": "/castle_on_the_grid/index.html"
+  }
+];
