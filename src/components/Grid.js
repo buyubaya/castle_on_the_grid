52be55b8a898ee8451be9fd42036efbcdf38d7ce
buyubaya <hihihi_caigikia@yahoo.com>
2019-01-19 22:02:55 -0800

    'HELLO'

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -11,18 +11,14 @@ class Grid extends Component {
             startCell: null,
             endCell: null,
             p: {},
-            startRadio: true,
-            endRadio: false,
-            blockRadio: false,
-            blocks: []
+            blocks: [],
+            isStart: false
         };
 
         this._start = this._start.bind(this);
-        this.handleChangeSize = this.handleChangeSize.bind(this);
+        this._find = this._find.bind(this);
+        this._reset = this._reset.bind(this);
         this.handleClickCell = this.handleClickCell.bind(this);
-        this.handleStartRadio = this.handleStartRadio.bind(this);
-        this.handleEndRadio = this.handleEndRadio.bind(this);
-        this.handleBlockRadio = this.handleBlockRadio.bind(this);
     }
 
     componentWillMount(){
@@ -185,53 +181,57 @@ class Grid extends Component {
         return tmp;
     }
 
-    handleChangeSize(e){
-        this.setState(
-            { size: e.target.value },
-            () => {
-                this._initGrid(this.state.size);
-            }
-        );
-    }
-
     handleClickCell(cell){
-        let { grid, startRadio, endRadio, blockRadio, blocks } = this.state;
+        let { grid, blocks } = this.state;
 
-        if(startRadio){
+        if(this.startRadio.checked){
             this.setState({ startCell: cell });
         }
 
-        if(endRadio){
+        if(this.endRadio.checked){
             this.setState({ endCell: cell });
         }
 
-        if(blockRadio){
-            grid[cell[0]][cell[1]] = null;
-            blocks.push(`${cell[0]}_${cell[1]}`);
+        if(this.blockRadio.checked){
+            if(grid[cell[0]][cell[1]] === null){
+                grid[cell[0]][cell[1]] = '.';
+                blocks = blocks.filter(item => item !== `${cell[0]}_${cell[1]}`);
+            }
+            else {
+                grid[cell[0]][cell[1]] = null;
+                blocks.push(`${cell[0]}_${cell[1]}`);
+            }
             this.setState({ grid, blocks });
         }
     }
 
-    handleStartRadio(){
-        this.setState({ startRadio: true, endRadio: false, blockRadio: false });
-    }
-
-    handleEndRadio(){
-        this.setState({ startRadio: false, endRadio: true, blockRadio: false });
-    }
-
-    handleBlockRadio(){
-        this.setState({ startRadio: false, endRadio: false, blockRadio: true });
-    }
-
-    _start(){
+    _find(){
         const { grid, startCell, endCell } = this.state;
         const p = this._minimumMoves(grid, startCell, endCell);
         this.setState({ p });
     }
 
+    _start(){
+        this.setState(
+            { isStart: true, size: this.sizeInput.value || 8 },
+            () => this._initGrid(this.sizeInput.value || 8)
+        );
+    }
+
+    _reset(){
+        this.setState({
+            size: 8,
+            grid: [],
+            startCell: null,
+            endCell: null,
+            p: {},
+            blocks: [],
+            isStart: false
+        });
+    }
+
 	render() {
-        const { grid, size, startCell, endCell, p, startRadio, endRadio, blockRadio, blocks } = this.state;
+        const { isStart, grid, size, startCell, endCell, p, blocks } = this.state;
         const styles = {
             grid: {
                 width: size * 50
@@ -244,74 +244,95 @@ class Grid extends Component {
 
 		return (
 			<div className="grid-wrapper">
-				<table className='board' style={styles.grid}>
-                    <tbody>
-                    {
-                        grid.map((row, i) =>
-                            <tr key={i}>
-                                {
-                                    row.map((cell, j) => {
-                                        const key = `${i}_${j}`;
-
-                                        return(
-                                            <td 
-                                                key={j}
-                                                className={blocks.indexOf(key) > -1 ? 'cell isNull' : 'cell'} 
-                                                style={styles.cell}
-                                                onClick={() => this.handleClickCell([i, j])}
-                                            >
-                                                {
-                                                    startCell && i === startCell[0] && j === startCell[1] &&
-                                                    <span className='startCell'></span>
-                                                }
-                                                {
-                                                    endCell && i === endCell[0] && j === endCell[1] &&
-                                                    <span className='endCell'></span>
-                                                }
-                                                {
-                                                    p[key] && p[key].indexOf('DOWN') > -1 &&
-                                                    <span className='line-up'></span>
-                                                }
-                                                {
-                                                    p[key] && p[key].indexOf('UP') > -1 &&
-                                                    <span className='line-down'></span>
-                                                }
-                                                {
-                                                    p[key] && p[key].indexOf('RIGHT') > -1 &&
-                                                    <span className='line-left'></span>
-                                                }
-                                                {
-                                                    p[key] && p[key].indexOf('LEFT') > -1 &&
-                                                    <span className='line-right'></span>
-                                                }
-                                            </td>
-                                        );
-                                    })
-                                }
-                            </tr>
-                        )
-                    }
-                    </tbody>
-                </table>
-			
-                <div className='options-area'>
-                    <input type='text' className='sizeNumber' onChange={this.handleChangeSize} />
-                    <div>
-                        <div>
-                            <span>Start Point</span>
-                            <input type='radio' name='cellType' className='cellType' defaultChecked={startRadio} onClick={this.handleStartRadio} />
+                {
+                    <div className={isStart ? 'form-start-area isHidden' : 'form-start-area'}>
+                        <div className='sizeNumber'>
+                            <input ref={el => this.sizeInput = el} type='text' defaultValue={8} />
                         </div>
-                        <div>
-                            <span>End Point</span>
-                            <input type='radio' name='cellType' className='cellType' defaultChecked={endRadio} onClick={this.handleEndRadio} />
-                        </div>
-                        <div>
-                            <span>Block</span>
-                            <input type='radio' name='cellType' className='cellType' defaultChecked={blockRadio} onClick={this.handleBlockRadio} />
-                        </div>
-                        <button className='btn-start' onClick={this._start}>Find Path</button>
+                        <button className='btn-start' onClick={this._start}>Start</button>
                     </div>
-                </div>
+                }
+				{
+                    isStart &&
+                    <div>
+                        <table className='board' style={styles.grid}>
+                            <tbody>
+                            {
+                                grid.map((row, i) =>
+                                    <tr key={i}>
+                                        {
+                                            row.map((cell, j) => {
+                                                const key = `${i}_${j}`;
+
+                                                return(
+                                                    <td 
+                                                        key={j}
+                                                        className={blocks.indexOf(key) > -1 ? 'cell isNull' : 'cell'} 
+                                                        style={styles.cell}
+                                                        onClick={() => this.handleClickCell([i, j])}
+                                                    >
+                                                        {
+                                                            startCell && i === startCell[0] && j === startCell[1] &&
+                                                            <span className='startCell'></span>
+                                                        }
+                                                        {
+                                                            endCell && i === endCell[0] && j === endCell[1] &&
+                                                            <span className='endCell'></span>
+                                                        }
+                                                        {
+                                                            p[key] && p[key].indexOf('DOWN') > -1 &&
+                                                            <span className='line-up'></span>
+                                                        }
+                                                        {
+                                                            p[key] && p[key].indexOf('UP') > -1 &&
+                                                            <span className='line-down'></span>
+                                                        }
+                                                        {
+                                                            p[key] && p[key].indexOf('RIGHT') > -1 &&
+                                                            <span className='line-left'></span>
+                                                        }
+                                                        {
+                                                            p[key] && p[key].indexOf('LEFT') > -1 &&
+                                                            <span className='line-right'></span>
+                                                        }
+                                                    </td>
+                                                );
+                                            })
+                                        }
+                                    </tr>
+                                )
+                            }
+                            </tbody>
+                        </table>
+                    
+                        <div className='options-area'>
+                            <div className='radio-area'>
+                                <div className='input-radio start'>
+                                    <label>
+                                        <input type='radio' name='cellType' className='cellType' defaultChecked={true} ref={el => this.startRadio = el} />
+                                        <span>Start Point</span>  
+                                    </label>
+                                </div>
+                                <div className='input-radio end'>
+                                    <label>
+                                        <input type='radio' name='cellType' className='cellType' ref={el => this.endRadio = el} />
+                                        <span>End Point</span>
+                                    </label>
+                                </div>
+                                <div className='input-radio block'>
+                                    <label>
+                                        <input type='radio' name='cellType' className='cellType' ref={el => this.blockRadio = el} />
+                                        <span>Block</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div className='btn-area'>
+                                <button className='btn-find' onClick={this._find}>Find Path</button>
+                                <button className='btn-reset' onClick={this._reset}>Reset</button>
+                            </div>
+                        </div>  
+                    </div>
+                }
             </div>
 		);
 	}
